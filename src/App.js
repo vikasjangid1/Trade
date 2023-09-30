26b787a662c8e74ebe701eb7e07142a1bb2f8404
Vikas Jangid <121178843+vikasjangid1@users.noreply.github.com>
2023-09-30 13:17:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TradingViewWidget from './TradingViewWidget';
+
+function App() {
+  return (
+    <div className="App">
+       <TradingViewWidget/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TradingViewWidget.jsx
@@ -0,0 +1,63 @@
+// TradingViewWidget.jsx
+
+import React, { useEffect, useRef } from 'react';
+
+let tvScriptLoadingPromise;
+
+export default function TradingViewWidget() {
+  const onLoadScriptRef = useRef();
+
+  useEffect(
+    () => {
+      onLoadScriptRef.current = createWidget;
+
+      if (!tvScriptLoadingPromise) {
+        tvScriptLoadingPromise = new Promise((resolve) => {
+          const script = document.createElement('script');
+          script.id = 'tradingview-widget-loading-script';
+          script.src = 'https://s3.tradingview.com/tv.js';
+          script.type = 'text/javascript';
+          script.onload = resolve;
+
+          document.head.appendChild(script);
+        });
+      }
+
+      tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+      return () => onLoadScriptRef.current = null;
+
+      function createWidget() {
+        if (document.getElementById('tradingview_0098e') && 'TradingView' in window) {
+          new window.TradingView.widget({
+            autosize: false,
+            symbol: "NASDAQ:AAPL",
+            timezone: "Etc/UTC",
+            theme: "light",
+            style: "1",
+            locale: "in",
+            enable_publishing: true,
+            withdateranges: true,
+            range: "YTD",
+            hide_side_toolbar: false,
+            allow_symbol_change: true,
+            details: true,
+            hotlist: true,
+            calendar: true,
+            show_popup_button: true,
+            popup_width: "1000",
+            popup_height: "650",
+            container_id: "tradingview_0098e"
+          });
+        }
+      }
+    },
+    []
+  );
+
+  return (
+    <div className='tradingview-widget-container'>
+      <div id='tradingview_0098e' />
+    </div>
+  );
+}
